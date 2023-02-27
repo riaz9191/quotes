3c4fdf222a33055846b61c44bd778687ba3e1a19
riaz9191 <riazahammed9191@gmail.com>
2023-02-27 09:43:35 +0600

    finsished

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 const loadData = () => {
     fetch('https://api.kanye.rest/')
-    .then(res => res.json())
-    .then(data => displayQuote(data))
+        .then(res => res.json())
+        .then(data => displayQuote(data))
 }
 const displayQuote = quote => {
     console.log(quote.quote);
-    const div= document.getElementById('q-body')
+    const div = document.getElementById('q-body')
     div.innerHTML = `
-    <div class="card text-center bg-dark text-white pb-3" style="width: 20rem;">
-                <div class="card-body">
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">${quote.quote} </p>
-                <button onclick="loadData();" type="button" class=" btn btn-outline-info">New Quote</button>
-                </div>
+    <div class="card w-96 bg-base-600 shadow-xl">
+                <div class="card-body ">
+                    <h6 class="card-subtitle mb-2 text-center font-bold text-2xl">Kanye Quotes</h6>
+                    <p class="card-text text-xl">${quote.quote} </p>
+                <div class="card-actions justify-center">
+      <button onclick="loadData();" class="btn btn-primary mt-5">New Quote</button>
+    </div>
+    </div>
             </div>
     `
 }
